ed949a6a5d93e832c217365d0c6686640c82a6f1
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-10-25 08:15:04 +0100

    unsigned species

--- data.sql
+++ data.sql
@@ -1,8 +1,8 @@
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species) VALUES
-('Charmander', '2020-02-08', 0, false, -11.0, 'Unspecified'),
-('Plantmon', '2021-11-15', 2, true, -5.7, 'Unspecified'),
-('Squirtle', '1993-04-02', 3, false, -12.13, 'Unspecified'),
-('Angemon', '2005-06-12', 1, true, -45.0, 'Unspecified'),
-('Boarmon', '2005-06-07', 7, true, 20.4, 'Unspecified'),
-('Blossom', '1998-10-13', 3, true, 17.0, 'Unspecified'),
-('Ditto', '2022-05-14', 4, true, 22.0, 'Unspecified');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES
+('Charmander', '2020-02-08', 0, false, -11.0),
+('Plantmon', '2021-11-15', 2, true, -5.7),
+('Squirtle', '1993-04-02', 3, false, -12.13),
+('Angemon', '2005-06-12', 1, true, -45.0),
+('Boarmon', '2005-06-07', 7, true, 20.4),
+('Blossom', '1998-10-13', 3, true, 17.0),
+('Ditto', '2022-05-14', 4, true, 22.0);
