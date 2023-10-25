e99700151deeeab3d252bf780cc06c0eea5cafea
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-10-25 08:32:09 +0100

    update changes

--- transaction.sql
+++ transaction.sql
@@ -1,9 +1,25 @@
-BEGIN;
+/* BEGIN;
 
 UPDATE animals SET species = 'Unspecified';
 
 SELECT * FROM animals;
 
 ROLLBACK;
+
+SELECT * FROM animals; */
+
+BEGIN;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL OR species = '';
+
+SELECT * FROM animals;
+
+COMMIT;
 
 SELECT * FROM animals;
