f45aebb20c6baa0dd5f3649a1da45ae3ec1400f5
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-10-31 16:08:22 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -27,14 +27,6 @@ INSERT INTO owners (full_name, age) VALUES
 ('Dean Winchester', 14),
 ('Jodie Whittaker', 38);
 
-INSERT INTO owners (full_name, age) VALUES
-('Sam Smith', 34),
-('Jennifer Orwell', 19),
-('Bob', 45),
-('Melody Pond', 77),
-('Dean Winchester', 14),
-('Jodie Whittaker', 38);
-
 INSERT INTO species (name) VALUES 
 ('Pokemon'), ('Digimon');
 
